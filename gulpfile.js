71139977e946288166b6e94376b40a409b5031f3
Tomas Bedrich <ja@tbedrich.cz>
2016-01-25 16:01:33 +0100

    Cleaned up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -10,77 +10,112 @@ var filter = require('gulp-filter');
 var plumber = require('gulp-plumber');
 var replace = require('gulp-replace');
 var git = require('git-rev-sync');
+var templateCache = require('gulp-angular-templatecache');
 
-var paths = {
+var appPaths = {
   styles: ['./scss/**/*.scss', './scss/**/*.css'],
   scripts: ['./app/**/*.js', './app/*.js'],
 };
 
-gulp.task('styles', function() {
-  gulp.src(paths.styles)
+var minifyCssOptions = {
+  keepSpecialComments: 0,
+};
+
+var injectSrcOptions = {
+  read: false,
+};
+
+var injectOptions = {
+  relative: true,
+};
+
+var bowerCssFilter = [
+  '*.css',
+  '!*.map.css',
+];
+
+var bowerJsFilter = [
+  '*',
+  '!source-map.js',
+  '!*.map.js',
+];
+
+gulp.task('styles', function(done) {
+  // copy bower component styles
+  gulp.src(mainBowerFiles())
+    .pipe(filter(bowerCssFilter))
+    .pipe(gulp.dest('./www/lib'));
+
+  // compile and minify app styles
+  gulp.src(appPaths.styles)
     .pipe(plumber())
     .pipe(sass())
-    .pipe(minifyCss({
-      keepSpecialComments: 0,
-    }))
+    .pipe(minifyCss(minifyCssOptions))
     .pipe(concat('style.min.css'))
     .pipe(gulp.dest('./www/css/'));
+
+  // inject
+  var libStream = gulp.src('./www/lib/**/*.css', injectSrcOptions);
+  var appStream = gulp.src('./www/css/**/*.css', injectSrcOptions);
+  gulp.src('./www/index.html')
+    .pipe(inject(series(libStream, appStream), injectOptions))
+    .pipe(gulp.dest('./www'));
+
+  done();
 });
 
-gulp.task('scripts', function() {
+gulp.task('scripts', function(done) {
 
-  // copy bower scripts
+  // copy bower component scripts
   gulp.src(mainBowerFiles())
-    .pipe(filter([
-      '*',
-      '!source-map.js',
-    ]))
+    .pipe(filter(bowerJsFilter))
     .pipe(gulp.dest('./www/lib'));
 
   // copy app scripts
-  gulp.src(paths.scripts)
-
-    // .pipe(concat('app.js'))
+  gulp.src(appPaths.scripts)
+    /*.pipe(concat('app.js'))*/
     .pipe(gulp.dest('./www/js'));
 
   // inject into index.html
   var libStream = gulp.src([
     './www/lib/angular.js',
-    './www/lib/Chart.js',
     './www/lib/angular*.js',
     './www/lib/ionic.js',
     './www/lib/ionic*.js',
     './www/lib/ng*.js',
     './www/lib/stacktrace.min.js',
     './www/lib/stack*.js',
+    './www/lib/Chart.js',
     './www/lib/*.js',
-    './www/lib/**/*.css',
-  ], {
-    read: false,
-  });
+  ], injectSrcOptions);
   var appStream = gulp.src([
     './www/js/services/*.js',
     './www/js/controllers/*.js',
     './www/js/directives/*.js',
     './www/js/**/*.js',
-    './www/css/**/*.css',
-  ], {
-    read: false,
-  });
+  ], injectSrcOptions);
   gulp.src('./www/index.html')
-    .pipe(inject(series(libStream, appStream), {
-      relative: true,
-    }))
+    .pipe(inject(series(libStream, appStream), injectOptions))
+    .pipe(gulp.dest('./www'));
+
+  done();
+});
+
+gulp.task('tag-git-head', function(done) {
+  gulp.src('./www/index.html')
     .pipe(replace(
       new RegExp('<!-- gulp-replace:git-head-sha:\\w+ -->'),
       '<!-- gulp-replace:git-head-sha:' + git.long() + ' -->'
-    )) // TODO separate to another task
+    ))
     .pipe(gulp.dest('./www'));
+
+  done();
 });
 
-gulp.task('watch', function() {
-  gulp.watch(paths.styles, ['styles']);
-  gulp.watch(paths.scripts, ['scripts']);
+gulp.task('watch', function(done) {
+  gulp.watch(appPaths.styles, ['styles']);
+  gulp.watch(appPaths.scripts, ['scripts']);
+  done();
 });
 
 gulp.task('default', ['scripts', 'styles']);
