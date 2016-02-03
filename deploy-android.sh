b5ae894905ac7ebb3233deafb37e8664ee94e60c
Tomas Bedrich <ja@tbedrich.cz>
2016-02-03 14:15:34 +0100

    Updated deploy script

--- deploy-android.sh
+++ deploy-android.sh
@@ -3,8 +3,13 @@
 set -x
 gulp
 ionic build --release android
+
 cd platforms/android/build/outputs/apk
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/.android/release.keystore android-release-unsigned.apk release
-rm android-release.apk
-/Applications/android-sdk-macosx/build-tools/*/zipalign -v 4 android-release-unsigned.apk android-release.apk
+rm android-*-release.apk
+
+for FILE in android-*-unsigned.apk; do
+  jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/.android/release.keystore $FILE release
+  /Applications/android-sdk-macosx/build-tools/*/zipalign -v 4 $FILE ${FILE%-*}-signed.apk
+done
+
 open .
