3a1fe62982038c1378f7bf744e26d9407bcc3073
Tomas Bedrich <ja@tbedrich.cz>
2016-02-10 15:16:36 +0100

    Added monthly chart

--- app/controllers/history.js
+++ app/controllers/history.js
@@ -2,33 +2,59 @@
 
 var module = angular.module('experience.controllers.history', []);
 
-var HistoryController = function($scope, storeService, dateFilter) {
+module.constant('historyChartLength', {
+  days: 14,
+  months: 12,
+});
+
+var HistoryController = function($scope, storeService, dateFilter, historyChartLength) {
   $scope.user = storeService.getUser();
   $scope.lessons = null;
+  $scope.historyChartLength = historyChartLength;
+  $scope.grouping = null;
+
+  var groupingFn = {
+    days: function(d1, d2) {
+      return (d1.getDate() == d2.getDate() && d1.getMonth() == d2.getMonth() && d1.getFullYear() == d2.getFullYear());
+    },
+
+    months: function(d1, d2) {
+      return (d1.getMonth() == d2.getMonth() && d1.getFullYear() == d2.getFullYear());
+    },
+  };
 
   var prepareChartData = function() {
-    var chartDayCount = 15; // in days
-
-    var isSameDay = function(d1, d2) {
-      return (d1.getDate() == d2.getDate() && d1.getMonth() == d2.getMonth() && d1.getFullYear() == d2.getFullYear());
-    };
 
     $scope.chartLabels = [];
     $scope.chartData = [
       [],
     ];
 
+    var currentLesson = $scope.lessons.length - 1; // oldest lesson
+
+    // set initial date
     var day = new Date();
-    day.setDate(day.getDate() - chartDayCount);
-    var currentLesson = $scope.lessons.length - 1;
+    if ($scope.grouping == 'days') {
+      day.setDate(day.getDate() - historyChartLength.days);
+    } else if ($scope.grouping == 'months') {
+      day.setMonth(day.getMonth() - historyChartLength.months, 1);
+    }
 
-    for (var i = 0; i < chartDayCount; i++) {
-      day.setDate(day.getDate() + 1);
-      $scope.chartLabels.push(dateFilter(day, 'shortDate'));
+    var numberOfGroups = $scope.grouping == 'days' ? historyChartLength.days : historyChartLength.months;
+    for (var i = 0; i < numberOfGroups; i++) {
 
-      // take all lessons for this day and sum their score
+      // generate date group and add label
+      if ($scope.grouping == 'days') {
+        day.setDate(day.getDate() + 1);
+        $scope.chartLabels.push(dateFilter(day, 'shortDate'));
+      } else if ($scope.grouping == 'months') {
+        day.setMonth(day.getMonth() + 1);
+        $scope.chartLabels.push(dateFilter(day, 'MMM y'));
+      }
+
+      // take all lessons for this group and sum their score
       var sum = 0;
-      for (; currentLesson >= 0 && isSameDay(day, new Date($scope.lessons[currentLesson].startTime)); currentLesson--) {
+      for (; currentLesson >= 0 && groupingFn[$scope.grouping](day, new Date($scope.lessons[currentLesson].startTime)); currentLesson--) {
         sum += $scope.lessons[currentLesson].score;
       }
 
@@ -36,10 +62,15 @@ var HistoryController = function($scope, storeService, dateFilter) {
     }
   };
 
+  $scope.changeGrouping = function(type) {
+    $scope.grouping = type;
+    prepareChartData();
+  };
+
   $scope.$on('$ionicView.beforeEnter', function() {
     storeService.getAllLessons().then(function(lessons) {
       $scope.lessons = lessons;
-      prepareChartData();
+      $scope.changeGrouping('days');
     });
   });
 };
