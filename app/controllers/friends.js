decd3015a77d63bb92699de9267a3c0b5752ee74
Tomas Bedrich <ja@tbedrich.cz>
2016-02-15 12:42:42 +0100

    Prepared custom profile in friend list

--- app/controllers/friends.js
+++ app/controllers/friends.js
@@ -5,10 +5,41 @@ var module = angular.module('experience.controllers.friends', []);
 var FriendsController = function ($scope, $ionicPlatform, storeService, userService) {
   $scope.friends = storeService.getUser().friends;
 
+  var loadUserItem = function () {
+
+    // get current week
+    var startDate = new Date();
+    var endDate = new Date();
+    var weekDay = startDate.getDay();
+    startDate.setDate(startDate.getDate() - weekDay);
+    endDate.setDate(endDate.getDate() + 6 - weekDay);
+
+    // load lessons between startDate and endDate
+    storeService.getLessonsBetween(startDate.getTime(), endDate.getTime()).then(function (lessons) {
+      var score = 0;
+      var duration = 0;
+
+      // sum score and duration
+      for (var i = 0; i < lessons.length; i++) {
+        score += lessons[i].score;
+        duration += lessons[i].duration;
+      }
+
+      $scope.friends.facebook.push({
+        id: 'me',
+        name: storeService.getUser().name,
+        score: score,
+      });
+
+    });
+  };
+
   $ionicPlatform.ready(function () {
     if (storeService.getUser().provider == 'facebook') {
       userService.loadFriendsFacebook();
     }
+
+    // loadUserItem();
   });
 };
 
