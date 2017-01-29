831b8e8cdde9461d7d50f1aa4473e14af91e55ea
Tomas Bedrich <ja@tbedrich.cz>
2017-01-29 18:38:00 +0100

    Updated version to 1.0.1

--- app/app.js
+++ app/app.js
@@ -47,7 +47,7 @@ module.config(function ($qProvider) {
 });
 
 // TODO make automatic from package.json or config.xml
-module.constant('appVersion', '1.0.0');
+module.constant('appVersion', '1.0.1');
 
 module.run(function ($ionicConfig, $ionicPlatform, $ionicHistory, $rootScope, lessonService, fxeService, syncService) {
 
